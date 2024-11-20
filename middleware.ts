2f446fbbb61531ddab46f98d559311eb107d06a7
Mirko Bozzetto <mirkouch@gmail.com>
2024-11-20 19:03:42 +0100

    feat: enhance middleware config matcher to exclude additional file types for improved routing in Edge Runtime

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { auth as middleware } from "@src/lib/auth/auth";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|\\.svg|\\.png|\\.jpg|public).*)"],
 };
